8bad2ce0ac5416230db6d3d65e456a9451d414ab
scottshanfeld <74272645+scottshanfeld@users.noreply.github.com>
2022-04-24 21:18:16 -0400

    Update minesweeper.py

--- AI-Minesweeper/AI-Minesweeper/minesweeper.py
+++ AI-Minesweeper/AI-Minesweeper/minesweeper.py
@@ -1,3 +1,222 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler #normalize and scale feature data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from tensorflow.keras.models import load_model
+
+import itertools
+import random
+import numpy as ny
+import pandas as pd
+import seaborn as sns
+
+
+
+class Minesweeper():
+    """
+    Minesweeper game representation
+    """
+
+    def __init__(self, height=8, width=8, mines=8):
+
+        # Set initial width, height, and number of mines
+        self.height = height
+        self.width = width
+        self.mines = set()
+
+        # Initialize an empty field with no mines
+        self.board = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                row.append(False)
+            self.board.append(row)
+
+        # Add mines randomly
+        while len(self.mines) != mines:
+            i = random.randrange(height)
+            j = random.randrange(width)
+            if not self.board[i][j]:
+                self.mines.add((i, j))
+                self.board[i][j] = True
+
+        # At first, player has found no mines
+        self.mines_found = set()
+
+    def print(self):
+        """
+        Prints a text-based representation
+        of where mines are located.
+        """
+        for i in range(self.height):
+            print("--" * self.width + "-")
+            for j in range(self.width):
+                if self.board[i][j]:
+                    print("|X", end="")
+                else:
+                    print("| ", end="")
+            print("|")
+        print("--" * self.width + "-")
+
+    def is_mine(self, cell):
+        i, j = cell
+        return self.board[i][j]
+
+    def nearby_mines(self, cell):
+        """
+        Returns the number of mines that are
+        within one row and column of a given cell,
+        not including the cell itself.
+        """
+
+        # Keep count of nearby mines
+        count = 0
+
+        # Loop over all cells within one row and column
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+
+                # Ignore the cell itself
+                if (i, j) == cell:
+                    continue
+
+                # Update count if cell in bounds and is mine
+                if 0 <= i < self.height and 0 <= j < self.width:
+                    if self.board[i][j]:
+                        count += 1
+
+        return count
+
+    def won(self):
+        """
+        Checks if all mines have been flagged.
+        """
+        return self.mines_found == self.mines
+
+    def getSurrounding(self,cell):
+        queue = []
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+
+                # Ignore the cell itself
+                if (i, j) == cell:
+                    continue
+
+                # Update count if cell in bounds and is mine
+                if 0 <= i < self.height and 0 <= j < self.width:
+                    queue.append((i, j))
+
+        return queue
+
+    def scan_section(self,start, size, revealed):
+        board_data = []
+        for i in range(size):
+            x = i + start[0]
+            for j in range(size):
+                y = j + start[1]
+                if(x >= self.height or y >= self.width): #marks area outside board as -2
+                    board_data.append(-2)
+                elif (self.is_mine((x, y))):  # marks bomb tiles as -1
+                    board_data.append(-1)
+                elif((x,y) in revealed):
+                    board_data.append(self.nearby_mines((x,y))) #marks tile with number of bombs surround otherwise
+                else:
+                    board_data.append(-3) #markes tile with -3 if unknown
+        return(board_data)
+
+    def scan_all_sections(self, size, revealed):
+        sections = []
+        for i in range(0,self.height, size):
+            for j in range(0, self.width, size):
+                sections.append(self.scan_section((i,j),size, revealed))
+        return sections
+
+    def scan_all_sections_with_safes(self, size, revealed):
+        sections = []
+        section = []
+        safes_section = []
+        for i in range(0,self.height, size):
+            for j in range(0, self.width, size):
+                section = self.scan_section((i,j),size, revealed)
+                safes_section = []
+                for k in range(len(section)):
+                    if(section[k] == -1):
+                        section[k] = -3
+                        safes_section.append(0)
+                    else:
+                        safes_section.append(1)
+                sections.append(section + safes_section)
+        return sections
+
+
+
+class Sentence():
+    """
+    Logical statement about a Minesweeper game
+    A sentence consists of a set of board cells,
+    and a count of the number of those cells which are mines.
+    """
+
+    def __init__(self, cells, count):
+        self.cells = set(cells)
+        self.count = count
+
+    def __eq__(self, other):
+        return self.cells == other.cells and self.count == other.count
+
+    def __str__(self):
+        return f"{self.cells} = {self.count}"
+
+    def known_mines(self):
+        """
+        Returns the set of all cells in self.cells known to be mines.
+        """
+        known_mines = set()
+        if len(self.cells)==self.count:
+            for cell in self.cells:
+                #cell = True
+                known_mines.add(cell)
+            #print(f'known_mines is {known_mines}')
+            return known_mines
+        else:
+            return set()
+
+
+    def known_safes(self):
+        """
+        Returns the set of all cells in self.cells known to be safe.
+        """
+        known_safes = set()
+        if self.count==0:
+            for cell in self.cells:
+                #cell = False
+                known_safes.add(cell)
+            #print(f'known_safes is {known_safes}')
+            return known_safes
+        else:
+            return set()
+
+
+    def mark_mine(self, cell):
+        """
+        Updates internal knowledge representation given the fact that
+        a cell is known to be a mine.
+        """
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
+
+
+    def mark_safe(self, cell):
+        """
+        Updates internal knowledge representation given the fact that
+        a cell is known to be safe.
+        """
+        if cell in self.cells:
+            self.cells.remove(cell)
+
+class MinesweeperAI(): #This is the main function to edit
     """
     Minesweeper game player
     """
@@ -137,53 +356,38 @@
                 return (i,j)
         return None
 
-    #def make_nn_move(self,board_section):
-        #model.load_model('my_minesweeper_model.h5')
-        #board_state = pd.DataFrame([board_section], columns=['(0;0)', '(1;0)', '(2;0)', '(3;0)', '(0;1)', '(1;1)', '(2;1)', '(3;1)', '(0;2)', '(1;2)', '(2;2)', '(3;2)', '(0;3)', '(1;3)', '(2;3)', '(3;3)'])
-        #probs = model.predict(board_state).tolist()
-        #return max(probs)
+    def make_nn_move(self,board_section,i,j):
+        board_section = [-3 if item == -1 else item for item in board_section] #replaces bomb tiles with unknown
+        print(board_section)
+        if board_section == [-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3]: #board with no info
+            print("EMPTY")
+            return
+        import tensorflow as tf
+        from tensorflow import keras
+        model = tf.keras.models.load_model('my_minesweeper_model.h5')
 
-    '''def NN(self, gridVector):
-        from sklearn.model_selection import train_test_split
-        from sklearn.preprocessing import MinMaxScaler #normalize and scale feature data
-        from tensorflow.keras.models import Sequential
-        from tensorflow.keras.layers import Dense
-        from sklearn.metrics import mean_absolute_error, mean_squared_error
-        import numpy as ny
-        import pandas as pd
-        import seaborn as sns
-        df=pd.read_csv('board_data.csv')
-        df.head()
-        x=df[['(0;0)', '(1;0)', '(2;0)', '(3;0)', '(0;1)', '(1;1)', '(2;1)', '(3;1)', '(0;2)', '(1;2)', '(2;2)', '(3;2)', '(0;3)', '(1;3)', '(2;3)', '(3;3)']].values
-        y=df[['s(0;0)', 's(1;0)', 's(2;0)', 's(3;0)', 's(0;1)', 's(1;1)', 's(2;1)', 's(3;1)', 's(0;2)', 's(1;2)', 's(2;2)', 's(3;2)', 's(0;3)', 's(1;3)', 's(2;3)', 's(3;3)']].values
-        n = x.shape[1] # n is amount of features being processed
-        x_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=102)
-        scaler = MinMaxScaler()
-        scaler.fit(x_train)
-        x_train = scaler.transform(x_train)
-        X_test = scaler.transform(X_test)
-        model = Sequential()
-        model.add(Dense((10 * n), activation='relu'))  # 1st layer -- input layer
-        model.add(Dense((20 * n), activation='relu'))  # 2nd layer
-        model.add(Dense((10 * n), activation='relu'))  # 3rd layer
-        model.add(Dense((5 * n), activation='sigmoid'))   # 4th layer
-        model.add(Dense(n))                            # output (may change n to 1 if we decide to only have 1 output. Currently set up to output a percent chance of a bomb at a given space)
-        model.compile(optimizer='rmsprop', loss='mse') # 'mse'-> mean square error
-        model.fit(x=x_train, y=y_train, epochs=500)    #epochs -> number of pass over the entired dataset ->this is where neural network is run
-        loss_df = pd.DataFrame(model.history.history)
-        #Evaluation of Data -- will show us how often ai fails game (clicked a bomb)
-        model.evaluate(X_test, y_test, verbose=0) # returns mean square error
-        model.evaluate(x_train, y_train, verbose=0)
-        test_predictions = model.predict(X_test)
-        test_predictions = pd.Series(test_predictions.flatten()) #converting to dataframe
-        pred_df = pd.DataFrame(y_test, columns=['orig s(0;0)', 'orig s(1;0)', 'orig s(2;0)', 'orig s(3;0)', 'orig s(0;1)', 'orig s(1;1)', 'orig s(2;1)', 'orig s(3;1)', 'orig s(0;2)', 'orig s(1;2)', 'orig s(2;2)', 'orig s(3;2)', 'orig s(0;3)', 'orig s(1;3)', 'orig s(2;3)', 'orig s(3;3)'])
-        pred_df = pd.concat([pred_df, test_predictions], axis=1)
-        pred_df.columns = ['orig s(0;0)', 'orig s(1;0)', 'orig s(2;0)', 'orig s(3;0)', 'orig s(0;1)', 'orig s(1;1)', 'orig s(2;1)', 'orig s(3;1)', 'orig s(0;2)', 'orig s(1;2)', 'orig s(2;2)', 'orig s(3;2)', 'orig s(0;3)', 'orig s(1;3)', 'orig s(2;3)', 'orig s(3;3)', 'Predictions']
-        #Saves model
-        from tensorflow.keras.models import load_model
-        model.save('my_minesweeper_model.h5')
-        #loads model
-        my_model = load_model('my_minesweeper_model.h5')  
-        board_state = pd.DataFrame ([[-3,-3,-3,-3,-3,1,-3,4,-3,-3,-3,-3,-3,-3,-3,-3]], columns = ['(0;0)', '(1;0)', '(2;0)', '(3;0)', '(0;1)', '(1;1)', '(2;1)', '(3;1)', '(0;2)', '(1;2)', '(2;2)', '(3;2)', '(0;3)', '(1;3)', '(2;3)', '(3;3)'])
-        model.predict(board_state)        
-'''
+        board_state = pd.DataFrame([board_section], columns=['(0;0)', '(1;0)', '(2;0)', '(3;0)', '(0;1)', '(1;1)', '(2;1)', '(3;1)', '(0;2)', '(1;2)', '(2;2)', '(3;2)', '(0;3)', '(1;3)', '(2;3)', '(3;3)'])
+        probs = model.predict(board_state).tolist()
+        print(probs)
+        maxProb = -9999
+        maxCoord = (0,0)
+        for ndx in range(len(probs[0])):
+            y = int(ndx / 4)
+            x = ndx % 4
+            coord = (x, y)
+            if(((y+j, x+i) not in self.moves_made) and (probs[0][ndx] > maxProb)):
+                print(probs[0][ndx], ": ", coord)
+                maxProb = probs[0][ndx]
+                maxCoord = coord
+        print("Move Within Section: ", (maxCoord))
+
+        if maxProb != -9999:
+            print("index:", probs[0].index(maxProb))
+            return maxCoord
+        else:
+            print("No moves in this section.")
+
+
+
+
+
